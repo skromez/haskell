02d6f64d74c4146210616f593512a2bc82699fb4
Dmitry <ddnovikov@vtb.ru>
2021-04-18 14:20:10 +0300

    refactor

--- Factorial.hs
+++ Factorial.hs
@@ -13,3 +13,9 @@ factorial2 n | n == 0 = 1
              | otherwise = error "arg must be >= 0"
 
 doubleFactorial n = if n - 2 <= 0 then n else n * doubleFactorial(n - 2)
+
+factorial3 n | n >= 0 = helper 1 n
+             | otherwise = error "arg must be >= 0"
+
+helper acc 0 = acc
+helper acc n = helper (acc * n) (n - 1)
--- Fibonacci.hs
+++ Fibonacci.hs
@@ -1,5 +1,18 @@
 fibonacci :: Integer -> Integer
 fibonacci 0 = 0
 fibonacci 1 = 1
-fibonacci n | n < 0 = fibonacci(n+2) - fibonacci(n+1)
-            | n > 0 = fibonacci(n-1) + fibonacci(n-2)
+fibonacci n | n > 0 = fibonacci(n - 1) + fibonacci(n - 2)
+            | n < 0 = fibonacci(n + 2) - fibonacci(n + 1)
+
+
+fibonacciWithHelper :: Integer -> Integer
+fibonacciWithHelper 0 = 0
+fibonacciWithHelper 1 = 1
+fibonacciWithHelper n | n <= 0 = helperMinus 1 0 n
+                      | n > 1 = helper 1 0 n
+
+helper acc prev 0 = prev
+helper acc prev next = helper prev (acc + prev) (next - 1)
+
+helperMinus acc prev 0 = prev
+helperMinus acc prev next = helperMinus prev (acc - prev) (next + 1)
